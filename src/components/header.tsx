56111eb4cdf9ab0cd9ae5503ffbeb645e623b775
ppacman <smwfkim2@naver.com>
2024-02-04 14:01:42 +0900

    feat. MainPage.tsx 퍼블리싱 #3

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,35 +1,69 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+
 const Header = () => {
   return (
-    <>
-      <Navigation>
-        <Logo>
-          <img src="" alt="logo" />
-        </Logo>
-        <NavItem href="#">스터디/프로젝트</NavItem>
-        <NavItem href="#">멘토링</NavItem>
-        <NavItem href="#">커뮤니티</NavItem>
-      </Navigation>
-    </>
+    <Wrapper>
+      <Typo>ItEat</Typo>
+      <MenuWrapper>
+        <MenuDiv>스터디/프로젝트</MenuDiv>
+      </MenuWrapper>
+    </Wrapper>
   );
 };
 
 export default Header;
 
-const Navigation = styled.nav`
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100px;
   background-color: white;
-  padding: 30px 0;
-  text-align: center;
   display: flex;
+  align-items: center;
+  gap: 50px;
 `;
 
-const Logo = styled.p``;
-const NavItem = styled.a`
-  color: Black;
-  text-decoration: none;
-  margin: 0 10px;
+const Typo = styled.div`
+  font-size: 48px;
+  color: #5649ea;
+  font-style: italic;
+  font-weight: 700;
+`;
+
+const MenuWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const MenuDiv = styled.div`
+  font-size: 16px;
+  color: black;
+  font-weight: 500;
+  cursor: pointer;
+`;
+
+const ExtendedMenu = styled.div`
+  position: absolute;
+  top: 100%; /* 메뉴 아래에 위치하도록 설정 */
+  left: 0;
+  width: 150px; /* 메뉴의 폭 설정 */
+  background-color: #fff; /* 배경색 설정 */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* 그림자 설정 */
+  z-index: 1; /* 다른 요소 위에 표시되도록 설정 */
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const ExtendedMenuItem = styled.div`
+  font-size: 14px;
+  color: black;
+  cursor: pointer;
   &:hover {
-    text-decoration: underline;
+    color: #5649ea; /* 호버 효과 설정 */
   }
 `;
