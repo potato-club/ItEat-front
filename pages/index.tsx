322ac642df16a43c9337d50cce204a6282232598
ppacman <smwfkim2@naver.com>
2024-02-21 11:34:09 +0900

    fix. mainPage index 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import MainPage from "@/container/mainPage/MainPage";
-
+import Header from "@/components/Header";
 export default function Home() {
-  return <MainPage />;
+  return (
+    <>
+      <MainPage />
+    </>
+  );
 }
