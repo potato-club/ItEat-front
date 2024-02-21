5cd23c540bda29ca1b69f848a7f880b4debc0627
ppacman <smwfkim2@naver.com>
2024-02-21 11:39:05 +0900

    feat. MainPage.tsx 퍼블리싱

--- src/container/mainPage/MainPage.tsx
+++ src/container/mainPage/MainPage.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Header from "../../components/Header";
 import styled from "styled-components";
+import RecentProject from "./components/RecentContent";
+import PopularProject from "./components/PopularContent";
+
 const MainPage = () => {
   return (
-    <Wrapper>
+    <>
       <Header />
-    </Wrapper>
+      <Wrapper>
+        <Banner />
+        <RecentProject />
+        <PopularProject />
+      </Wrapper>
+    </>
   );
 };
 
 export default MainPage;
 
 const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
   background-color: white;
+  display: flex;
+  flex-direction: column;
+  gap: 150px;
+  z-index: 1;
+`;
+
+const Banner = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: aliceblue;
 `;
