4d88b65b454ed36c57957a66b0809837ca38b7c8
kim jihyeon <jihyeon546@gmail.com>
2024-04-10 11:39:48 +0900

    remove: signupPage#2

--- pages/signup/signupmain.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SignUpContainer from "../../src/container/SignUpPage/SignUpMain";
-
-const SignUpMain = () => {
-  return <SignUpContainer />;
-};
-
-export default SignUpMain;
